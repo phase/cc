70b3226f65e4eac20e7b2c88c28eecc48266b90a
Jadon Fowler <jadonflower@gmail.com>
2017-03-18 13:33:06 -0700

    Fix dimensions and move the Turtle back to the start
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- quarry.lua
+++ quarry.lua
@@ -3,28 +3,37 @@ local quarry = function(length, width, depth)
   local mineRow = function()
     turtle.digDown()
     turtle.down()
-    for i=1,length do
+    for i = 1, (length - 1) do
       turtle.dig()
       turtle.forward()
     end
-    for i=1,length do
+    for i = 1, (length - 1) do
       turtle.back()
     end
   end
 
   local mineSlice = function()
-    for i=1,depth do
+    for i = 1, (depth - 1) do
       mineRow()
     end
-    for i=1,depth do
+    for i = 1, (depth - 1) do
       turtle.up()
     end
   end
 
-  for i=1,width do
+  for i = 1, (width - 1) do
     mineSlice()
-    turtle.left()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.forward()
+    turtle.turnRight()
   end
+
+  turtle.turnRight()
+  for i = 1, (width - 1) do
+    turtle.forward()
+  end
+  turtle.turnLeft()
 
 end
 
